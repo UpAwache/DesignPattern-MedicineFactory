4946af89ecbda7f5a69a99e00bdc34646ab02aef
UpAwache <133492553+UpAwache@users.noreply.github.com>
2024-01-01 23:20:42 +0800

    Fine和Rough Producer的修改

--- MedicineFactory/src/main/java/Manufacturing/ProductLine/Producer/FineProducer.java
+++ MedicineFactory/src/main/java/Manufacturing/ProductLine/Producer/FineProducer.java
@@ -5,16 +5,14 @@ import Presentation.Protocol.IOManager;
 /**
  * 细加工生产方式类.
  *
- * @author 孟繁霖
- * @date 2021-10-12 8:55
  */
 public class FineProducer implements ProduceManner {
     @Override
     public void produce() {
         IOManager.getInstance().print(
-                "细加工:切块去核->冰糖小火熬制->加入添加剂->大火熬制->出锅密封保存",
-                "細加工:切塊去核->冰糖小火熬製->加入添加劑->大火熬製->出鍋密封保存",
-                "Fine processing: cut into pieces and remove core -> rock sugar boiled on low fire -> added additives -> boiled on high fire -> sealed and stored out of the pot"
+                "细加工:药材处理->药物提取->熬制->反应与处理->过滤与纯化->质量检测->包装与储存",
+                "细加工:药材处理->药物提取->熬制->反应与处理->过滤与纯化->质量检测->包装与储存",
+                "Fine processing: Herbal Material Processing -> rock sugar boiled on low fire -> added additives -> boiled on high fire -> sealed and stored out of the pot"
         );
     }
 }
--- MedicineFactory/src/main/java/Manufacturing/ProductLine/Producer/RoughProducer.java
+++ MedicineFactory/src/main/java/Manufacturing/ProductLine/Producer/RoughProducer.java
@@ -5,16 +5,14 @@ import Presentation.Protocol.IOManager;
 /**
  * 粗加工生产类
  *
- * @author 孟繁霖
- * @date 2021-10-12 8:49
  */
 public class RoughProducer implements ProduceManner {
     @Override
     public void produce() {
 
         IOManager.getInstance().print(
-                "粗加工:切块去核->倒入调味剂->熬制->出锅密封",
-                "粗加工:切塊去核->倒入調味劑->熬製->出鍋密封",
-                "Rough processing: cut into pieces and remove the core -> pour in the flavoring -> boil -> seal out the pot");
+                "粗加工:药材处理->药物提取->熬制->反应与处理->包装与储存",
+                "粗加工:药材处理->药物提取->熬制->反应与处理->包装与储存",
+                "Rough processing: Herbal Material Processing -> pour in the flavoring -> boil -> seal out the pot");
     }
 }
